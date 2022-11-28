c65e76f8d1867323518f0d87e7014edc249ee3e8
Tom E <guacaplushy@icloud.com>
2022-11-27 21:01:10 -0500

    fix

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,8 +7,9 @@ finaldir=$HOME/.ios_cc_toolchain
 # sdk download
 mkdir $workdir/SDK
 wget https://github.com/xybp888/iOS-SDKs/releases/download/iOS-SDKs/iPhoneOS16.1.sdk.zip -O $workdir/iPhoneOS16.1.sdk.zip
-unzip $workdir/iPhoneOS16.1.sdk.zip -o -d $outdir/SDK
-rm -rf $workdir/iPhoneOS16.1.sdk.zip
+unzip $workdir/iPhoneOS16.1.sdk.zip -o -d $workdir/iPhoneOS16.1.sdk
+tar caf $workdir/iPhoneOS16.1.sdk.tar.xz $workdir/iPhoneOS16.1.sdk
+rm -rf $workdir/iPhoneOS16.1.sdk.zip $workdir/iPhoneOS16.1.sdk
 
 
 echo "clang -target aarch64-apple-ios15 -miphoneos-version-min=15.0 -arch arm64 -arch arm64e -fuse-ld=lld -isysroot $finaldir/SDK/iPhoneOS.sdk \$@" > $outdir/aarch64-apple-ios15-clang
@@ -19,7 +20,7 @@ echo "echo \"\$@\" | grep "\-o" | awk '{split($0, fArr, "-o "); split(fArr[2], s
 git clone --recursive https://github.com/guacaplushy/cctools-port $workdir/cctools
 
 cd $workdir/cctools/usage_examples/ios_toolchain
-./build.sh $outdir/SDK arm64
+./build.sh $workdir/iPhoneOS16.1.sdk.tar.xz arm64
 mv target $outdir
 mv $outdir $finaldir
 find $outdir -type f -print0 -exec sudo ln -s {} /usr/local/bin/{} \;
