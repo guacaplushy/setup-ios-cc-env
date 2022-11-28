f09fe4d04947068accc65783ea90ee98f60cbd00
Tom E <guacaplushy@icloud.com>
2022-11-27 23:39:58 -0500

    k

--- bootstrap.sh
+++ bootstrap.sh
@@ -14,10 +14,10 @@ tar caf $workdir/iPhoneOS16.1.sdk.tar.xz $workdir/iPhoneOS16.1.sdk
 rm -rf $workdir/iPhoneOS16.1.sdk.zip $workdir/iPhoneOS16.1.sdk
 
 
-echo "clang -target aarch64-apple-ios -miphoneos-version-min=15.0 -arch arm64 -arch arm64e -fuse-ld=lld -isysroot $finaldir/SDK/iPhoneOS.sdk \$@" > $outdir/aarch64-apple-ios-clang
-echo "ldid -s \$(echo \"\$@\" | grep "\-o" | awk '{split($0, fArr, "-o "); split(fArr[2], sArr, " "); print sArr[2]}'\)" >> $outdir/aarch64-apple-ios-clang
-echo "clang++ -target aarch64-apple-ios -miphoneos-version-min=15.0 -arch arm64 -arch arm64e -fuse-ld=lld -stdlib=libc++ -isysroot $finaldir/SDK/iPhoneOS.sdk \$@" > $outdir/aarch64-apple-ios-clang++
-echo "echo \"\$@\" | grep "\-o" | awk '{split($0, fArr, "-o "); split(fArr[2], sArr, " "); print sArr[2]}'" >> $outdir/aarch64-apple-ios-clang++
+echo "clang -target aarch64-apple-ios -miphoneos-version-min=15.0 -arch arm64 -arch arm64e -fuse-ld=lld -isysroot $finaldir/SDK/iPhoneOS.sdk \$@" > $outdir/bin/aarch64-apple-ios-clang
+echo "ldid -s \$(echo \"\$@\" | grep "\-o" | awk '{split($0, fArr, "-o "); split(fArr[2], sArr, " "); print sArr[2]}'\)" >> $outdir/bin/aarch64-apple-ios-clang
+echo "clang++ -target aarch64-apple-ios -miphoneos-version-min=15.0 -arch arm64 -arch arm64e -fuse-ld=lld -stdlib=libc++ -isysroot $finaldir/SDK/iPhoneOS.sdk \$@" > $outdir/bin/aarch64-apple-ios-clang++
+echo "echo \"\$@\" | grep "\-o" | awk '{split($0, fArr, "-o "); split(fArr[2], sArr, " "); print sArr[2]}'" >> $outdir/bin/aarch64-apple-ios-clang++
 
 git clone --recursive https://github.com/guacaplushy/cctools-port $workdir/cctools
 
