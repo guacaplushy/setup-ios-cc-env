c3b6df3115e42f86ea783c97069a1d0c2808804b
Tom E <guacaplushy@icloud.com>
2022-11-28 00:51:37 -0500

    less verbose

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,34 +1,47 @@
 #!/usr/bin/env bash
+echo "iOS cross compiler installer for Linux"
+echo "Made by guacaplushy"
 
 workdir=$(mktemp -d)
 outdir=$(mktemp -d)
 finaldir=$HOME/.ios_cc_toolchain
+rm -rf $finaldir
 mkdir -p $finaldir
 mkdir -p $finaldir/bin
 
+echo "[*] Installing dependencies"
+sudo apt install libplist2.0-dev cmake clang build-essential unzip -y > /dev/null
+
 # sdk download
+echo "[*] Downloading \& extracting SDK"
 mkdir $workdir/SDK
-wget https://github.com/xybp888/iOS-SDKs/releases/download/iOS-SDKs/iPhoneOS16.1.sdk.zip -O $workdir/iPhoneOS16.1.sdk.zip
-unzip -o $workdir/iPhoneOS16.1.sdk.zip -d $workdir/iPhoneOS16.1.sdk
-tar caf $workdir/iPhoneOS16.1.sdk.tar.xz $workdir/iPhoneOS16.1.sdk
+wget https://github.com/xybp888/iOS-SDKs/releases/download/iOS-SDKs/iPhoneOS16.1.sdk.zip -O $workdir/iPhoneOS16.1.sdk.zip > /dev/null
+unzip -o $workdir/iPhoneOS16.1.sdk.zip -d $workdir/iPhoneOS16.1.sdk > /dev/null
+echo "[*] Repacking SDK"
+tar caf $workdir/iPhoneOS16.1.sdk.tar.xz $workdir/iPhoneOS16.1.sdk > /dev/null
 rm -rf $workdir/iPhoneOS16.1.sdk.zip $workdir/iPhoneOS16.1.sdk
 
-
+echo "[*] Installing wrappers"
 echo "clang -target aarch64-apple-ios -miphoneos-version-min=15.0 -arch arm64 -arch arm64e -fuse-ld=lld -isysroot $finaldir/SDK/iPhoneOS.sdk \$@" > $outdir/bin/aarch64-apple-ios-clang
 echo "ldid -s \$(echo \"\$@\" | grep "\-o" | awk '{split($0, fArr, "-o "); split(fArr[2], sArr, " "); print sArr[2]}'\)" >> $outdir/bin/aarch64-apple-ios-clang
 echo "clang++ -target aarch64-apple-ios -miphoneos-version-min=15.0 -arch arm64 -arch arm64e -fuse-ld=lld -stdlib=libc++ -isysroot $finaldir/SDK/iPhoneOS.sdk \$@" > $outdir/bin/aarch64-apple-ios-clang++
 echo "echo \"\$@\" | grep "\-o" | awk '{split($0, fArr, "-o "); split(fArr[2], sArr, " "); print sArr[2]}'" >> $outdir/bin/aarch64-apple-ios-clang++
 
-git clone --recursive https://github.com/guacaplushy/cctools-port $workdir/cctools
+echo "[*] Cloning cctools"
+git clone --recursive https://github.com/guacaplushy/cctools-port $workdir/cctools > /dev/null
 
 cd $workdir/cctools/usage_examples/ios_toolchain
-./build.sh $workdir/iPhoneOS16.1.sdk.tar.xz arm64
+echo "[*] Compiling cctools"
+./build.sh $workdir/iPhoneOS16.1.sdk.tar.xz arm64 > /dev/null
 mv target/* $outdir
-git clone https://github.com/ProcursusTeam/ldid $workdir/ldid
+echo "[*] Compiling ldid"
+git clone https://github.com/ProcursusTeam/ldid $workdir/ldid > /dev/null
 cd $workdir/ldid
-make -j2
-DESTDIR=$outdir make install
+make -j2 > /dev/null
+DESTDIR=$outdir make install > /dev/null 
+echo "[*] Finishing up"
 rm -rf $workdir
 mv $outdir/* $finaldir
 rm -rf $outdir
 echo "export PATH=$finaldir/bin:\$PATH" >> $HOME/.bashrc
+echo "[*] Done! If your shell is not bash, you must add $finaldir to your PATH in your shell's rc file."
